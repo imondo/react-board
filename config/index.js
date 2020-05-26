eabb704a0629e6b9506a0aa4c033ba769b8a1217
Mondo (via Travis CI) <wuyw@sqqmall.com>
2020-05-26 07:53:44 +0000

    Deploy one-pupil/react-board to github.com/one-pupil/react-board.git:gh-pages

--- config/index.js
+++
@@ -1,7 +0,0 @@
-const dev = require('./dev')
-const prod = require('./prod')
-
-module.exports = {
-  dev,
-  prod
-}
--- config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: 'production',
-  API: '/api', // 接口
-  publicPath: '/react-board' // 输出路径
-}
